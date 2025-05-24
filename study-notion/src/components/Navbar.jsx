276e3feca2bca2ab7fb37ec1fcb66aef3b1df653
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2025-05-25 01:01:57 +0530

    Done with navbar and toast

--- study-notion/src/components/Navbar.jsx
+++ study-notion/src/components/Navbar.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import Logo from '../assets/logo.png'
 import { Link } from 'react-router-dom'
+import toast from 'react-hot-toast'
 
-const Navbar = ({isLoggedin}) => {
+const Navbar = ({isLoggedin, setLoggedin}) => {
 
   return (
     <div className='w-full flex bg-blue-950 p-2 justify-evenly items-center'>
@@ -29,28 +30,32 @@ const Navbar = ({isLoggedin}) => {
         <div className='flex text-white ml-6 gap-6'>
             { !isLoggedin &&
                 <Link to='/login'>
-                    <button className='ring px-4 py-1 rounded-md hover:ring-blue-600'>
+                    <button className='ring px-4 py-1 rounded-md cursor-pointer hover:ring-blue-600'>
                         Login
                     </button>
                 </Link>
             }
             { !isLoggedin &&
                 <Link to='/signup'>
-                    <button className='ring px-4 py-1 rounded-md hover:ring-blue-600'>
+                    <button className='ring px-4 py-1 rounded-md cursor-pointer hover:ring-blue-600'>
                         Sign Up
                     </button>
                 </Link>
             }
             { isLoggedin &&
                 <Link to='/'>
-                    <button className='ring px-4 py-1 rounded-md hover:ring-blue-600'>
+                    <button className='ring px-4 py-1 rounded-md cursor-pointer hover:ring-blue-600'
+                        onClick={() => {
+                            setLoggedin(false)
+                            toast.success('Logged Out')
+                            }}>
                         Logout
                     </button>
                 </Link>
             }
             { isLoggedin &&
                 <Link to='/dashboard'>
-                    <button className='ring px-4 py-1 rounded-md hover:ring-blue-600'>
+                    <button className='ring px-4 py-1 rounded-md cursor-pointer hover:ring-blue-600'>
                         Dashboard
                     </button>
                 </Link>
