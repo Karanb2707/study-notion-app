deaec48889083e8a6362427b8710f8f8042cc45b
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2025-05-24 23:49:34 +0530

    route setup

--- study-notion/src/App.jsx
+++ study-notion/src/App.jsx
@@ -1,10 +1,23 @@
-
+import { Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Dashboard from './pages/Dashboard'
 
 function App() {
 
   return (
-    <div className="text-center">
-      Hello
+    <div>
+      <Navbar />
+
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+
+      </Routes>
     </div>
   )
 }
